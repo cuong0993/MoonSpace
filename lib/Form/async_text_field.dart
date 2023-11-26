c589a19395b5f7d247c4a5a13bfc34948ca35662
Hari Krishnan <144345505+codeharik@users.noreply.github.com>
2023-11-26 23:36:12 +0530

    AsyncTextFormField

--- lib/Form/async_text_field.dart
+++ lib/Form/async_text_field.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:moonspace/helper/stream/functions.dart';
 import 'package:moonspace/helper/validator/debug_functions.dart';
 
+typedef AsyncText = ({String? error, bool load});
+
 class AsyncTextFormField extends StatefulWidget {
   const AsyncTextFormField({
     super.key,
@@ -15,35 +17,45 @@ class AsyncTextFormField extends StatefulWidget {
     this.autofocus = false,
     this.showPrefix = true,
     this.milliseconds = 300,
+    this.textAlign = TextAlign.start,
+    this.textInputAction,
+    this.onSubmit,
   });
 
   final TextEditingController? con;
   final Future<String?> Function(String value) asyncValidator;
-  final InputDecoration? decoration;
+  final InputDecoration Function(AsyncText asyncText, TextEditingController textCon)? decoration;
   final String? initialValue;
   final bool autofocus;
   final bool showPrefix;
   final int milliseconds;
   final TextStyle? style;
+  final TextAlign textAlign;
+  final TextInputAction? textInputAction;
+  final void Function(String value)? onSubmit;
 
   @override
   State<AsyncTextFormField> createState() => _AsyncTextFormFieldState();
 }
 
 class _AsyncTextFormFieldState extends State<AsyncTextFormField> {
-  ({String? valid, bool load})? asyncText;
+  AsyncText asyncText = (error: null, load: false);
   final focusNode = FocusNode(debugLabel: 'AsyncTextFormField');
   final key = GlobalKey<FormFieldState>();
+
+  late TextEditingController textCon;
 
   late final StreamController<String> fnStream;
 
   @override
   void initState() {
+    textCon = widget.con ?? TextEditingController(text: widget.initialValue);
+
     fnStream = createDebounceFunc(widget.milliseconds, (String name) async {
-      asyncText = (valid: 'Checking...', load: true);
+      asyncText = (error: 'Checking...', load: true);
       setState(() {});
       key.currentState?.validate();
-      asyncText = (valid: (await widget.asyncValidator(name)), load: false);
+      asyncText = (error: (await widget.asyncValidator(name)), load: false);
       setState(() {});
       key.currentState?.validate();
     });
@@ -60,16 +72,16 @@ class _AsyncTextFormFieldState extends State<AsyncTextFormField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: widget.con,
+      controller: textCon,
       key: key,
-      initialValue: widget.initialValue,
+      textAlign: widget.textAlign,
 
       autofocus: widget.autofocus,
 
       //
       onTapOutside: (event) => focusNode.unfocus(),
       onEditingComplete: () => owl('onEditingComplete'),
-      onFieldSubmitted: (value) => owl('onFieldSubmitted'),
+      onFieldSubmitted: widget.onSubmit,
       onSaved: (v) => owl(v),
       onTap: () => owl('onTap'),
       //
@@ -78,32 +90,31 @@ class _AsyncTextFormFieldState extends State<AsyncTextFormField> {
       //
       style: widget.style,
 
-      decoration: widget.decoration?.copyWith(
-        isDense: true,
-        prefixIcon: !widget.showPrefix
-            ? null
-            : Container(
-                alignment: Alignment.center,
-                width: 20,
-                height: 20,
-                margin: const EdgeInsets.all(16),
-                child: (asyncText?.valid == null)
-                    ? const Icon(Icons.done)
-                    : (asyncText?.load == true
-                        ? const CircularProgressIndicator()
-                        : (asyncText == null
-                            ? const Icon(Icons.account_circle_outlined)
-                            : const Icon(Icons.error_outline))),
-              ),
-      ),
+      decoration: widget.decoration?.call(asyncText, textCon).copyWith(
+            isDense: true,
+            prefixIcon: !widget.showPrefix
+                ? null
+                : Container(
+                    alignment: Alignment.center,
+                    width: 20,
+                    height: 20,
+                    margin: const EdgeInsets.all(16),
+                    child: (asyncText.error == null)
+                        ? const Icon(Icons.done)
+                        : (asyncText.load == true
+                            ? const CircularProgressIndicator()
+                            : const Icon(Icons.error_outline)),
+                  ),
+          ),
       onChanged: (value) => fnStream.add(value),
       validator: (value) {
         lava(value);
-        if ((asyncText?.load ?? false)) {
+        if ((asyncText.load)) {
           return 'Checking';
         }
-        return asyncText?.valid;
+        return asyncText.error;
       },
+      textInputAction: TextInputAction.done,
     );
   }
 }
--- lib/helper/extensions/color.dart
+++ lib/helper/extensions/color.dart
@@ -130,3 +130,18 @@ extension AsHtmlColorToColor on String {
     return null; // Return null if parsing fails
   }
 }
+
+Color lerpGradient(List<Color> colors, List<double> stops, double t) {
+  if (stops.length != colors.length) return Colors.red;
+  for (var s = 0; s < stops.length - 1; s++) {
+    final leftStop = stops[s], rightStop = stops[s + 1];
+    final leftColor = colors[s], rightColor = colors[s + 1];
+    if (t <= leftStop) {
+      return leftColor;
+    } else if (t < rightStop) {
+      final sectionT = (t - leftStop) / (rightStop - leftStop);
+      return Color.lerp(leftColor, rightColor, sectionT)!;
+    }
+  }
+  return colors.last;
+}
